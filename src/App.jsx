d98b8f1528bd9e1562cc3c48d87dc9dc2e45b570
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-11-14 18:16:31 -0500

    feat: desafio 1 done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hola Mundo React!!! 👋</h1>
+      <hr />
+      <p>Done with 💪 by Alex Marin Mendez on November 2021</p>
+    </>
+  );
+};
+
+export default App;
